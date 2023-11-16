fec33444a3241f5eb587b831f3c8ab034e83b0e6
alireza <alirezaghorbani.bmedu@yahoo.com>
2023-11-16 15:06:47 +0100

    multiply two numbers

---
+++ php-beginner/multiplier.php
@@ -0,0 +1,8 @@
+<?php
+echo "Ik ga twee getallen met elkaar vermenigvuldigen" . PHP_EOL;
+$number1 = readline("Eerste getal?");
+$number2 = readline("Tweede getal?");
+
+$result = $number1 * $number2;
+echo "Uw resltaat: $result";
+?>
