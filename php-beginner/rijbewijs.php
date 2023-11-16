66a8d99d830bf70ee09ea0aba4fd5ef85bfbc5fa
alireza <alirezaghorbani.bmedu@yahoo.com>
2023-11-16 16:45:40 +0100

    if-statement

---
+++ php-beginner/rijbewijs.php
@@ -0,0 +1,9 @@
+<?php
+$leeftijd = readline("Hoe oud ben jij? ") . PHP_EOL;
+if($leeftijd >= 16.5){
+    echo ("Je mag biginnen met rijlessen!" . PHP_EOL);
+    }else {
+        echo ("Helaas, je mag nog niet beginnen met rijlessen." . PHP_EOL);
+    }
+
+?>
