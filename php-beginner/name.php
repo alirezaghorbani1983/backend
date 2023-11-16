fce1a7927f66a814ff32c3d827187c43aca41594
alireza <alirezaghorbani.bmedu@yahoo.com>
2023-11-16 14:28:13 +0100

    new

---
+++ php-beginner/name.php
@@ -0,0 +1,7 @@
+<?php
+
+$voornaam = readline("Wat is je voornaam?" . PHP_EOL);
+$achternaam = readline("Wat is je achternaam?" . PHP_EOL);
+
+echo "Jouw naam is:" . $voornaam . " " . $achternaam . PHP_EOL;
+?>
