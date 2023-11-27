ba9d669d71389d0937d46c7a3b3f35a699a7a2c5
alireza <alirezaghorbani.bmedu@yahoo.com>
2023-11-27 22:52:17 +0100

    finall-editie

---
+++ PHP/php-novice/bucket-list.php
@@ -0,0 +1,23 @@
+<?php
+
+$aantal = readline("Hoeveel activiteiten wil je toevoegen?" . PHP_EOL);
+
+if (!is_numeric($aantal)) {
+    exit("'$aantal' is geen getal, probeer het opnieuw" . PHP_EOL);
+}
+
+$activiteiten = array();
+
+for ($i = 1; $i <= $aantal; $i++) {
+    $activiteit = readline("Wat wil jij toevoegen aan jouw bucket list?" . PHP_EOL);
+    $activiteiten[] = $activiteit; // Voeg de activiteit toe aan de array
+}
+
+echo "Op jouw bucket list staat: " . PHP_EOL;
+
+foreach ($activiteiten as $activiteit) {
+    echo $activiteit . PHP_EOL;
+}
+
+?>
+
